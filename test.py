4355a8112c5d788753a9701b058153868c916f63
Marcovo93 <marcovolpe93@gmail.com>
2026-02-06 14:52:05 +0100

    init 1.3

--- test.py
+++ test.py
@@ -3,14 +3,15 @@ import os
 
 def light_my_path():
     file_found = 0
-
     if os.path.exists(path):
         if not os.path.isdir(path):
             print("that's not a folder")
         else:
             for roots, dirs, files in os.walk(path):
-                print(files)
-                file_found += 1
+                for file in files:
+                    percorso = os.path.join(roots, file)
+                    print(percorso)
+                    file_found += 1
             print(f"file found {file_found}")
     else:
         print("path not found")
