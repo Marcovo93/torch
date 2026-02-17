972fd4d172ff4591a258e2dcb1c1e5cd302ad112
Marcovo93 <marcovolpe93@gmail.com>
2026-02-17 16:04:24 +0100

    import glob module
    insert format filter on sys.argv[2]
    delete os.walk() line

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ import os
 import glob
 
 def light_my_path(path = sys.argv[1], format = sys.argv[2]):
-    file_found = 0
     if os.path.exists(path):
         if not os.path.isdir(path):
             print("that's not a folder")
         else:
-            file_count = 0
-            file = glob.glob(f"{path}/**/*.{format}", recursive=True, include_hidden=True)
-            for p in file:
-                file_count += 1
-                print(p)
-            print(file_count)
+            for roots, dirs, files in os.walk(path):
+                for file in files:
+                    full_path_file = os.path.join(roots, file)
+                    print(full_path_file)
+                    file_found += 1
+            print(f"file found {file_found}")
     else:
         print("path not found")
 
