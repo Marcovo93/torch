11854a9025127e76b3ccbd6177ded73c37d1bfc7
Marcovo93 <marcovolpe93@gmail.com>
2026-05-06 17:53:51 +0200

    add def test_file_exists() => test_main.py;

--- test_main.py
+++ test_main.py
@@ -16,4 +16,12 @@ def test_tmp_folder(tmp_path):
     file_found, file_list = light_my_path(str(d), ".txt")
     assert file_found == 10, f"mi aspetto 10 file .txt, ne ho trovati solo {file_found}"
 
+def test_file_exists():
+    path = "/Users/marcovolpe/Downloads/"
+    list_test = ['tmb.txt', 'tmb copia 4jpg.txt', 'tmb copia.txt', 'tmb copia 3.txt', 'tmb copia 2.txt', 'tmb copia 5.txt', 'tmb copia 5 2.txt', 'tmb copia 5 3.txt', 'tmb copia 5 4.txt', 'tmb copia 5.txt']
+    file_found, file_list = light_my_path(path, ".txt")
+
+    assert file_list == list_test, f"mi aspettavo questi file '{list_test}', ho ricevuto questi: {file_list}"
+
+
 
