b4123d486be6fa633fea16caff4022333e7d2db9
Marcovo93 <marcovolpe93@gmail.com>
2026-05-11 18:10:22 +0200

    add light_my_log(); => light.py

--- light.py
+++ light.py
@@ -14,24 +14,7 @@ def light_my_path(path, format):
                         full_path = os.path.join(roots, file)
                         file_list.append(file.lower())
                         print(full_path)
-    #log management
-            tmp_nfl = open('light_my_log','a')
-            anno_mese_giorno, ore_minuti = light_my_date()
-            tmp_nfl.write('\n')
-            tmp_nfl.write('------')
-            tmp_nfl.write('\n')
-            tmp_nfl.write(f'Log {anno_mese_giorno} at {ore_minuti}')
-            tmp_nfl.write('\n')
-            tmp_nfl.write('\n')
-            i = 0
-            for file in file_list:
-                i += 1
-                tmp_nfl.write(f'{file}\n')
-            tmp_nfl.write(f'Total file => {i}')
-            tmp_nfl.write('\n')
-            tmp_nfl.write('------')
-            tmp_nfl.write('\n')
-            tmp_nfl.close()
+            light_my_log(file_list)
             return file_list
     else:
         raise FileNotFoundError(f"Il percorso {path} non esiste!")
@@ -42,3 +25,22 @@ def light_my_date():
     anno_mese_giorno, ora_centesimi = str(time_now).split(' ')
     ore_minuti, centesimi = ora_centesimi.split('.')
     return anno_mese_giorno, ore_minuti
+
+def light_my_log(file_list):
+    tmp_nfl = open('light_my_log', 'a')
+    anno_mese_giorno, ore_minuti = light_my_date()
+    tmp_nfl.write('\n')
+    tmp_nfl.write('------')
+    tmp_nfl.write('\n')
+    tmp_nfl.write(f'Log {anno_mese_giorno} at {ore_minuti}')
+    tmp_nfl.write('\n')
+    tmp_nfl.write('\n')
+    i = 0
+    for file in file_list:
+        i += 1
+        tmp_nfl.write(f'{file}\n')
+    tmp_nfl.write(f'Total file => {i}')
+    tmp_nfl.write('\n')
+    tmp_nfl.write('------')
+    tmp_nfl.write('\n')
+    tmp_nfl.close()
