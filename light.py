2a930407a0ce5009dd99ba2383a59bf42a624924
Marcovo93 <marcovolpe93@gmail.com>
2026-05-13 16:18:22 +0200

    add more verbosity log

--- light.py
+++ light.py
@@ -4,32 +4,53 @@ from datetime import datetime
 
 def light_my_path(path, extension):
     file_list = []
+    yy_mm_dd, hour_minutes = date_hour()
+    log_file = open('light_my_log','a')
     if os.path.exists(path):
         if os.path.isdir(path):
-            yy_mm_dd, hour_minutes = date_hour()
-            tmp_nfl = open('light_my_log','a')
-            tmp_nfl.write('------')
-            tmp_nfl.write('\n')
-            tmp_nfl.write(f'Log {yy_mm_dd} - {hour_minutes}')
-            tmp_nfl.write('\n')
-            tmp_nfl.write('\n')
+            log_file.write('------')
+            log_file.write('\n')
+            log_file.write(f'Log {yy_mm_dd} - {hour_minutes}')
+            log_file.write('\n')
+            log_file.write('\n')
             for roots, dirs, files in os.walk(path):
                 for file in files:
                     if file.lower().endswith(extension.lower()):
                         full_path = os.path.join(roots, file)
                         file_list.append(file.lower())
-                        tmp_nfl.write(file+'\n')
+                        log_file.write(file+'\n')
                         print(full_path)
             len_list = len(file_list)
-            tmp_nfl.write(f'Total file found => {len_list}')
-            tmp_nfl.write('\n')
-            tmp_nfl.write('------')
-            tmp_nfl.close()
+            log_file.write('\n')
+            log_file.write(f'Path: {path}\n')
+            log_file.write(f'Extension => {extension}\n')
+            log_file.write(f'Total file found => {len_list}')
+            log_file.write('\n')
+            log_file.write('------')
+            log_file.close()
             return file_list
         else:
-            raise FileNotFoundError(f"Il percorso {path} non è una cartella")
+            log_file.write('------')
+            log_file.write('\n')
+            log_file.write(f'Log {yy_mm_dd} - {hour_minutes}')
+            log_file.write('\n')
+            log_file.write('\n')
+            log_file.write(f"FileNotFoundError: The path {path} is not a folder")
+            log_file.write('\n')
+            log_file.write('------')
+            log_file.close()
+            raise FileNotFoundError(f"The path {path} is not a folder")
     else:
-        raise FileNotFoundError(f"Il percorso {path} non esiste!")
+        log_file.write('------')
+        log_file.write('\n')
+        log_file.write(f'Log {yy_mm_dd} - {hour_minutes}')
+        log_file.write('\n')
+        log_file.write('\n')
+        log_file.write(f"FileNotFoundError: The path {path} doesn't exist!")
+        log_file.write('\n')
+        log_file.write('------')
+        log_file.close()
+        raise FileNotFoundError(f"The path {path} doesn't exist!")
 
 def log_file(file):
     tmp_nfl = open('light_my_log','a')
