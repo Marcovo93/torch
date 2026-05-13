53c96b030d95d7927275d0ecebba85af47f33d49
Marcovo93 <marcovolpe93@gmail.com>
2026-05-13 15:21:31 +0200

    clean some row; more verbosity in variable description;

--- light.py
+++ light.py
@@ -2,22 +2,20 @@ import sys
 import os
 from datetime import datetime
 
-def light_my_path(path, format):
+def light_my_path(path, extension):
     file_list = []
     if os.path.exists(path):
-        if not os.path.isdir(path):
-            raise FileNotFoundError(f"Il percorso {path} non è una cartella")
-        else:
-            anno_mese_giorno, ore_minuti = light_my_date()
+        if os.path.isdir(path):
+            yy_mm_dd, hour_minutes = date_hour()
             tmp_nfl = open('light_my_log','a')
             tmp_nfl.write('------')
             tmp_nfl.write('\n')
-            tmp_nfl.write(f'Log {anno_mese_giorno} - {ore_minuti}')
+            tmp_nfl.write(f'Log {yy_mm_dd} - {hour_minutes}')
             tmp_nfl.write('\n')
             tmp_nfl.write('\n')
             for roots, dirs, files in os.walk(path):
                 for file in files:
-                    if file.lower().endswith(format.lower()):
+                    if file.lower().endswith(extension.lower()):
                         full_path = os.path.join(roots, file)
                         file_list.append(file.lower())
                         tmp_nfl.write(file+'\n')
@@ -28,38 +26,18 @@ def light_my_path(path, format):
             tmp_nfl.write('------')
             tmp_nfl.close()
             return file_list
+        else:
+            raise FileNotFoundError(f"Il percorso {path} non è una cartella")
     else:
         raise FileNotFoundError(f"Il percorso {path} non esiste!")
 
-def light_my_log(file):
+def log_file(file):
     tmp_nfl = open('light_my_log','a')
     tmp_nfl.write(file+'\n')
-    return
 
 
-def light_my_date():
+def date_hour():
     time_now = datetime.now()
-    anno_mese_giorno, ora_centesimi = str(time_now).split(' ')
-    ore_minuti, centesimi = ora_centesimi.split('.')
-    return anno_mese_giorno, ore_minuti
-
-def light_my_log_list(file_list):
-    tmp_nfl = open('light_my_log', 'a')
-    anno_mese_giorno, ore_minuti = light_my_date()
-    tmp_nfl.write('\n')
-    tmp_nfl.write('------')
-    tmp_nfl.write('\n')
-    tmp_nfl.write(f'Log {anno_mese_giorno} at {ore_minuti}')
-    tmp_nfl.write('\n')
-    tmp_nfl.write('\n')
-    i = 0
-    for file in file_list:
-        i += 1
-        tmp_nfl.write(f'{file}\n')
-    tmp_nfl.write(f'Total file => {i}')
-    tmp_nfl.write('\n')
-    tmp_nfl.write('------')
-    tmp_nfl.write('\n')
-    tmp_nfl.close()
-
-#light_my_path('/Users/marcovolpe/Downloads/','jpg')
+    yy_mm_dd, hour_cent = str(time_now).split(' ')
+    hour_minutes, cent = hour_cent.split('.')
+    return yy_mm_dd, hour_minutes
