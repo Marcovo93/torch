0797d5f625325d9d40f1fa2d40f1a5c214f49774
Marcovo93 <marcovolpe93@gmail.com>
2026-05-13 17:49:07 +0200

    reduce verbosity log

--- light.py
+++ light.py
@@ -1,4 +1,3 @@
-import sys
 import os
 from datetime import datetime
 
@@ -11,31 +10,17 @@ def light_my_path(path, extension):
                     if file.lower().endswith(extension.lower()):
                         full_path = os.path.join(roots, file)
                         file_list.append(file.lower())
+                        log_file(file)
                         print(full_path)
-            len_list = len(file_list)
-            log_file(file_list, len_list, path, extension)
             return file_list
         else:
             raise FileNotFoundError(f"The path {path} is not a folder")
     else:
         raise FileNotFoundError(f"The path {path} doesn't exist!")
 
-def log_file(file_list, len_list, path, extension):
-    yy_mm_dd, hour_minutes = date_hour()
+def log_file(file):
     log_file = open('light_my_log','a')
-    log_file.write('------')
-    log_file.write('\n')
-    log_file.write(f'Log {yy_mm_dd} - {hour_minutes}')
-    log_file.write('\n')
-    log_file.write('\n')
-    for file in file_list:
-        log_file.write(file +'\n')
-    log_file.write('\n')
-    log_file.write(f'Path: {path}\n')
-    log_file.write(f'Extension => {extension}\n')
-    log_file.write(f'Total file found => {len_list}')
-    log_file.write('\n')
-    log_file.write('------')
+    log_file.write(file + '\n')
     log_file.close()
 
 
